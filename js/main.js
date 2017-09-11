7dc9c103307dd187e733deca233dedeb2145741c
Tom Mitchelmore <djt8000@outlook.com>
2017-09-11 22:34:29 +0100

    style switcher

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 // JavaScript Document
+//$('#blue').click(function (){
+//   $('link[href*="accent"]').attr('href','accent-blue.css');
+//});
+//$('#red').click(function (){
+//   $('link[href*="accent"]').attr('href','accent-red.css');
+//});
+//$('#green').click(function (){
+//   $('link[href*="accent"]').attr('href','accent-green.css');
+//});
+function blue(){
+document.getElementById("color").href="css/accent-blue.css";
+}
+function red(){
+document.getElementById("color").href="css/accent-red.css";
+}
+function green(){
+document.getElementById("color").href="css/accent-green.css";
+}
+function purple(){
+document.getElementById("color").href="css/accent-purple.css";
+}
+function pink(){
+document.getElementById("color").href="css/accent-pink.css";
+}
+function orange(){
+document.getElementById("color").href="css/accent-orange.css";
+}
+function grey(){
+document.getElementById("color").href="css/accent-grey.css";
+}
