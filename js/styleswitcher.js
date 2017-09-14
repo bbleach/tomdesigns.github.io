3647a02ee6442e9d530cdc5ab91ef1a1d198582d
Tom Mitchelmore <djt8000@outlook.com>
2017-09-14 20:40:42 +0100

    Add files via upload

--- js/styleswitcher.js
+++ js/styleswitcher.js
@@ -1,13 +1,3 @@
-// JavaScript Document
-//$('#blue').click(function (){
-//   $('link[href*="accent"]').attr('href','accent-blue.css');
-//});
-//$('#red').click(function (){
-//   $('link[href*="accent"]').attr('href','accent-red.css');
-//});
-//$('#green').click(function (){
-//   $('link[href*="accent"]').attr('href','accent-green.css');
-//});
 function blue(){
 document.getElementById("color").href="css/accent-blue.css";
 }
@@ -25,6 +15,12 @@ document.getElementById("color").href="css/accent-pink.css";
 }
 function orange(){
 document.getElementById("color").href="css/accent-orange.css";
+}
+function darkblue(){
+document.getElementById("color").href="css/accent-darkblue.css";
+}
+function white(){
+document.getElementById("color").href="css/accent-white.css";
 }
 function grey(){
 document.getElementById("color").href="css/accent-grey.css";
